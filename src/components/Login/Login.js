982da51a86a1918139896edd07c873f845756813
Martin Neshev <martinneshevsr@gmail.com>
2023-03-17 22:19:12 +0200

    - Upgraded Login form
    - Login form is now controlled

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,32 @@
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAt,  faLock, faUnlockKeyhole, faUser } from "@fortawesome/free-solid-svg-icons";
-
+import { faAt, faLock } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const emailIcon = <FontAwesomeIcon icon={faAt} />;
 const passwordIcon = <FontAwesomeIcon icon={faLock} />;
 
-
 const Login = () => {
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [errormsg, setErrorMsg] = useState();
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+
+        console.log(email + " " + password);
+    };
+
     return (
         <section className={styles["login-form-container"]}>
             <div className={styles["login-cta-container"]}>
@@ -18,24 +36,26 @@ const Login = () => {
                 </p>
             </div>
 
-            <form className={styles["login-form"]}>
+            <form className={styles["login-form"]} onSubmit={onSubmit}>
                 <div className={styles["login-email-container"]}>
-                <i className={styles["username-icon"]} >{emailIcon}</i>
-                    <input  
+                    <i className={styles["username-icon"]}>{emailIcon}</i>
+                    <input
                         type="email"
                         id="email"
-                        
+                        onChange={handleEmail}
+                        value={email}
                         name="email"
-                        placeholder="Email"
+                        placeholder="Youremail@here.com"
                     />
                 </div>
 
                 <div className={styles["login-password-container"]}>
-                <i className={styles["username-icon"]} >{passwordIcon}</i>
-                    <input 
+                    <i className={styles["username-icon"]}>{passwordIcon}</i>
+                    <input
                         type="password"
-                        id="form2Example2"
-                        
+                        id="password"
+                        onChange={handlePassword}
+                        value={password}
                         placeholder="Password"
                         name="password"
                     />
@@ -44,12 +64,12 @@ const Login = () => {
                 <button type="submit" className={styles["login-btn"]}>
                     LOGIN
                 </button>
+
+               
             </form>
 
             <div className={styles["login-notlogged-container"]}>
-                <p>
-                    Don't have an account? <Link to="/register" >Register</Link>
-                </p>
+              
             </div>
         </section>
     );
